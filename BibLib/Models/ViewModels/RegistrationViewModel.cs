24806157915139223276656f9323d78faf0011ef
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-13 19:38:59 +0300

    Added registration page+vievmodel

--- BibLib/Models/ViewModels/RegistrationViewModel.cs
+++ BibLib/Models/ViewModels/RegistrationViewModel.cs
@@ -4,9 +4,23 @@ namespace BibLib.Models.ViewModels
 {
     public class RegistrationViewModel
     {
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Электронная почта")]
+        [EmailAddress(ErrorMessage = "Неверный адрес")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Подтвердите электронную почту")]
+        [Compare("Email", ErrorMessage = "Адреса почты не совпадают")]
         public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
     }
 }
