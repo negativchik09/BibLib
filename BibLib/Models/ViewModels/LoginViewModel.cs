43792bad269a7a72c8c6f06412d050e9516426dc
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-14 12:18:55 +0300

    Added authorization form

--- BibLib/Models/ViewModels/LoginViewModel.cs
+++ BibLib/Models/ViewModels/LoginViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace BibLib.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibLib.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Электронная почта")]
+        [EmailAddress(ErrorMessage = "Неверный адрес")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
+        [Display(Name = "Запомнить меня")]
         public bool RememberMe { get; set; }
     }
 }
