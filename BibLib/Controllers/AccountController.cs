e04b96f2b063f9bd3b46e699114dd2f68cfcebe2
negativchik09 <zaika.bogdan2001@gmail.com>
2021-04-14 17:17:59 +0300

    Updated domain models, added templates for account interactions

--- BibLib/Controllers/AccountController.cs
+++ BibLib/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using BibLib.Domain;
 using BibLib.Domain.Entities;
 using BibLib.Models.ViewModels;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +14,12 @@ namespace BibLib.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly AppDbContext _ctx;
         public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<IdentityUser> signInManager, AppDbContext ctx)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _signInManager = signInManager;
             _ctx = ctx;
         }
@@ -25,34 +27,11 @@ namespace BibLib.Controllers
         [HttpGet]
         public IActionResult Registration(string returnUrl)
         {
-            if (User.Identity != null)
-            {
-                if (User.Identity.IsAuthenticated)
-                {
-                    return RedirectToAction("Profile", "Profile");
-                }
-            }
-            RegistrationViewModel model = new RegistrationViewModel
-            {
-                ListOfSecretQuestions = new List<string>
-                {
-                    "Девичья фамилия матери",
-                    "Кличка первого питомца",
-                    "Марка первой машины"
-                }
-            };
-            return View(model);
+            return View();
         }
         [HttpPost]
         public async Task<IActionResult> Registration(RegistrationViewModel model, string returnUrl)
         {
-            if (User.Identity != null)
-            {
-                if (User.Identity.IsAuthenticated)
-                {
-                    return RedirectToAction("Profile", "Profile");
-                }
-            }
             if (ModelState.IsValid)
             {
                 IdentityUser identityUser = await _userManager.FindByEmailAsync(model.Email);
@@ -84,77 +63,10 @@ namespace BibLib.Controllers
             }
             return View("Registration", model);
         }
-        
-        [HttpGet]
+
         public IActionResult Login(string returnUrl)
         {
-            if (User.Identity != null)
-            {
-                if (User.Identity.IsAuthenticated)
-                {
-                    return RedirectToAction("Profile", "Profile");
-                }
-            }
             return View();
         }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(string returnUrl, LoginViewModel model)
-        {
-            if (User.Identity != null)
-            {
-                if (User.Identity.IsAuthenticated)
-                {
-                    return RedirectToAction("Profile", "Profile");
-                }
-            }
-            if (ModelState.IsValid)
-            {
-                IdentityUser user = await _userManager.FindByEmailAsync(model.Email);
-                if (user != default)
-                {
-                    var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
-                    if (result.Succeeded)
-                    {
-                        await _signInManager.SignInAsync(user, model.RememberMe);
-                        return Redirect(returnUrl);
-                    }
-                    ModelState.AddModelError(nameof(LoginViewModel.Password), "Неверный пароль");
-                }
-                ModelState.AddModelError(nameof(LoginViewModel.Email), "Пользователя с такой почтой не существует");
-            }
-
-            return View("Login", model);
-        }
-
-        [HttpGet]
-        public IActionResult ForgotPassword()
-        {
-            return View("ForgotPassword");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                IdentityUser user = await _userManager.FindByEmailAsync(model.Email);                
-            }
-            return View("ForgotPassword", model);
-        }
-
-        [HttpGet]
-        public IActionResult SecurityCheck()
-        {
-            return View();
-        }
-        
-        [Authorize]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
-        
     }
 }
---
+++ BibLib/Migrations/20210414132442_ProfileImages.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BibLib.Migrations
+{
+    public partial class ProfileImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
