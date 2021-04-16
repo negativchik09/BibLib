d04953b3b5c6655d0c7ada65375d27ac409b05ac
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-16 12:58:55 +0300

    Edited ShortBookViewModel and added a default cover

--- BibLib/Models/ViewModels/ShortBookViewModel.cs
+++ BibLib/Models/ViewModels/ShortBookViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace BibLib.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibLib.Models.ViewModels
 {
     public class ShortBookViewModel
     {
-        
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Название")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+        [Display(Name = "Фотография обложки")]
+        public string Image { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Жанр")]
+        public string Genre { get; set; }
+        [Display(Name = "Серия")]
+        public string Series { get; set; }
+        public int NumberOfPages { get; set; }
+        public int Rating { get; set; }
+        public int Popularity { get; set; }
     }
 }
