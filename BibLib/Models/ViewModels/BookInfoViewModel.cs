d7250f004a35ab68a976d371f3122d524f934d87
negativchik09 <zaika.bogdan2001@gmail.com>
2021-04-16 15:03:14 +0300

    BookInfoViewModel

---
+++ BibLib/Models/ViewModels/BookInfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibLib.Models.ViewModels
+{
+    public class BookInfoViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Название")]
+        public string Title { get; set; }
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+        public string Image { get; set; }
+        [Display(Name = "Жанр")]
+        public string Genre { get; set; }
+        [Display(Name = "Серия")]
+        public string Series { get; set; }
+        public string Annotation { get; set; }
+        public string Text { get; set; }
+        public int NumberOfPages { get; set; }
+        public int Rating { get; set; }
+        public int Popularity { get; set; }
+    }
+}
