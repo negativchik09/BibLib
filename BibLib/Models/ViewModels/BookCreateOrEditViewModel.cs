cacf52bc4923e3d7e2258b2fe11bd2144d028388
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-16 12:38:53 +0300

    Added book add/create form

--- BibLib/Models/ViewModels/BookCreateOrEditViewModel.cs
+++ BibLib/Models/ViewModels/BookCreateOrEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 
 namespace BibLib.Models.ViewModels
@@ -7,12 +8,22 @@ namespace BibLib.Models.ViewModels
     {
         // hidden
         public int Id { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Название")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Автор")]
         public string Author { get; set; }
+        [Display(Name = "Фотография обложки")]
         public IFormFile Image { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Жанр")]
         public string Genre { get; set; }
+        [Display(Name = "Серия")]
         public string Series { get; set; }
         public int NumberOfPages { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Текст книги")]
         public IFormFile Text { get; set; }
         public int Rating { get; set; }
         public int Popularity { get; set; }
