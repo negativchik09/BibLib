1133149e200dd2cfd2793db09bbfe999b9f2bee5
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-16 20:41:03 +0300

    BookInfo updated

--- BibLib/Models/ViewModels/BookInfoViewModel.cs
+++ BibLib/Models/ViewModels/BookInfoViewModel.cs
@@ -5,21 +5,21 @@ namespace BibLib.Models.ViewModels
     public class BookInfoViewModel
     {
         public int Id { get; set; }
-        [Display(Name = "Название")]
+        [Display(Name = "Название: ")]
         public string Title { get; set; }
-        [Display(Name = "Автор")]
+        [Display(Name = "Автор: ")]
         public string Author { get; set; }
         public string Image { get; set; }
-        [Display(Name = "Жанр")]
+        [Display(Name = "Жанр: ")]
         public string Genre { get; set; }
-        [Display(Name = "Серия")]
+        [Display(Name = "Серия: ")]
         public string Series { get; set; }
-        [Display(Name = "Аннотация")]
+        [Display(Name = "Аннотация: ")]
         public string Annotation { get; set; }
         public string Text { get; set; }
-        [Display(Name = "Кол-во страниц")]
+        [Display(Name = "Кол-во страниц: ")]
         public int NumberOfPages { get; set; }
-        [Display(Name = "Рейтинг")]
+        [Display(Name = "Рейтинг: ")]
         public int Rating { get; set; }
         public int Popularity { get; set; }
     }
