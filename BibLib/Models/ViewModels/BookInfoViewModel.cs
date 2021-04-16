fae9cf51e47cf84a599addab5b5188d2239f8533
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-16 16:03:20 +0300

    Added BookInfo page

--- BibLib/Models/ViewModels/BookInfoViewModel.cs
+++ BibLib/Models/ViewModels/BookInfoViewModel.cs
@@ -14,9 +14,12 @@ namespace BibLib.Models.ViewModels
         public string Genre { get; set; }
         [Display(Name = "Серия")]
         public string Series { get; set; }
+        [Display(Name = "Аннотация")]
         public string Annotation { get; set; }
         public string Text { get; set; }
+        [Display(Name = "Кол-во страниц")]
         public int NumberOfPages { get; set; }
+        [Display(Name = "Рейтинг")]
         public int Rating { get; set; }
         public int Popularity { get; set; }
     }
