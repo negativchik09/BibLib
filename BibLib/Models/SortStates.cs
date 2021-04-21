9113a683e3bb21519246dca94a2b89a25b837b05
SobakaBalabaka <remember3oct11@gmail.com>
2021-04-21 22:45:56 +0300

    A lot of visual changes

--- BibLib/Models/SortStates.cs
+++ BibLib/Models/SortStates.cs
@@ -1,14 +1,24 @@
-﻿namespace BibLib.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibLib.Models.ViewModels
 {
     public enum SortStates
     {
+        [Display(Name = "По названию по возрастанию")]
         TitleAsc,
+        [Display(Name = "По названию по убыванию")]
         TitleDesc,
+        [Display(Name = "По популярности по возрастанию")]
         PopularityAsc,
+        [Display(Name = "По популярности по убыванию")]
         PopularityDesc,
+        [Display(Name = "По рейтингу по возрастанию")]
         RatingAsc,
+        [Display(Name = "По рейтингу по убыванию")]
         RatingDesc,
+        [Display(Name = "По серии по возрастанию")]
         SeriesAsc,
+        [Display(Name = "По серии по убыванию")]
         SeriesDesc
     }
 }
--- BibLib/Models/ViewModels/SearchViewModel.cs
+++ BibLib/Models/ViewModels/SearchViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace BibLib.Models.ViewModels
 {
     public class SearchViewModel
     {
         public List<ShortBookViewModel> List { get; set; }
+        [Display(Name = "Поиск без категории")]
         public string GeneralSearch { get; set; }
+        [Display(Name = "Название")]
         public string TitleInput { get; set; }
         public List<string> TitleDataSet { get; set; }
+        [Display(Name = "Автор")]
         public string AuthorInput { get; set; }
         public List<string> AuthorDataSet { get; set; }
+        [Display(Name = "Жанр")]
         public string GenreInput { get; set; }
         public List<string> GenreDataSet { get; set; }
+        [Display(Name = "Серия")]
         public string SeriesInput { get; set; }
         public List<string> SeriesDataSet { get; set; }
         public PaginationViewModel Pages { get; set; }
